c83d477d3e96c728c06118515c5bc436fbafce10
JoiltonFraga <69259334+JoiltonFraga@users.noreply.github.com>
2020-09-08 22:35:26 -0300

    exercicio spring

---
+++ Aula/Farmacia/Farmacia/src/main/java/com/ecommerce/Farmacia/repositoy/CategoriaRepository.java
@@ -0,0 +1,14 @@
+package com.ecommerce.Farmacia.repositoy;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ecommerce.Farmacia.model.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+	public List<Categoria> findALLByDescricaoContainingIgnoreCase (String descricao);
+}
